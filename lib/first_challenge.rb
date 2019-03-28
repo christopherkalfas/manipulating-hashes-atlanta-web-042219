af75faf82ea78ae79bc782aa971506ccbe2d6769
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-28 17:55:11 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,17 @@ def first_challenge
   }
 
   #your code here
-  
+  contacts.collect do |name, data|
+    data.collect do |attribute, value 
+      if value == :favorite_icecream_flavors
+        value.collect do |flavor|
+          if flavor.delete_if == "strawberry"
+          end
+        end
+      end
+    end
+  end 
+            
 
   #remember to return your newly altered contacts hash!
   contacts
